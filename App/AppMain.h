3a7e6db55298f36d233d07e50af4aad91d415a68
Sherlock Tang <sherlockshin@outlook.jp>
2024-05-12 13:27:21 +0900

    Initial Repository

---
+++ App/AppMain.h
@@ -0,0 +1,12 @@
+/*
+*@filename: AppMain.c
+*@note:
+*
+*/
+
+#ifndef __APPMAIN_H__
+#define __APPMAIN_H__
+
+extern void AppMain(void);
+
+#endif /*__APPMAIN_H__*/
---
+++ App/WT001.h
@@ -0,0 +1,5 @@
+/*
+*@filename:WT001.h
+*@note:
+*
+*/
